78fbc7b1e7dbbe64490d1d63e94d3a01f6c054ec
Rylie Pantua <ryliemartin_lstu@mstip.edu.ph>
2026-08-24 20:12:39 +0800

    Use deployed origin for API requests

--- frontend/public/js/api.js
+++ frontend/public/js/api.js
@@ -1,4 +1,6 @@
-let API_URL = 'http://localhost:3000/api';
+let API_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+    ? 'http://localhost:3000/api'
+    : `${window.location.origin}/api`;
 
 async function probeApiPorts() {
     const baseHost = 'http://localhost';
